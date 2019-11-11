b1ae5448643c4749fc6ba5bca3826372744a91ce
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-11-11 22:03:33 +0200

    chore(*): lint app

--- __tests__/test.js
+++ __tests__/test.js
@@ -5,9 +5,12 @@ import init from '../src/init';
 beforeEach(async () => {
   const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
   const html = await fs.readFile(pathToHtml, 'utf8');
+
+  // eslint-disable-next-line no-undef
   document.body.innerHTML = html;
 });
 
+// eslint-disable-next-line jest/expect-expect
 test('init', () => {
   init();
 });
--- src/init.js
+++ src/init.js
@@ -2,7 +2,9 @@ import '@babel/polyfill';
 import Example from './Example';
 
 export default () => {
+  // eslint-disable-next-line no-undef
   const element = document.getElementById('point');
   const obj = new Example(element);
+
   obj.init();
 };
