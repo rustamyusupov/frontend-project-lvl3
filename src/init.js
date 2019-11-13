5a570175687aa9e62e0103716c56104af6f299f5
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-11-13 22:46:59 +0200

    refactor(index, init): remove unused from js

--- src/init.js
+++ src/init.js
@@ -1,10 +1,3 @@
 import '@babel/polyfill';
-import Example from './Example';
 
-export default () => {
-  // eslint-disable-next-line no-undef
-  const element = document.getElementById('point');
-  const obj = new Example(element);
-
-  obj.init();
-};
+export default () => {};
