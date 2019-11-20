6456b633bf24e3d9207054c1368a035f4bea2dbe
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-11-20 22:29:53 +0200

    feat(init): init form listeners for input, blur and submit

--- src/init.js
+++ src/init.js
@@ -1,3 +1,35 @@
-import '@babel/polyfill';
+/* eslint-disable no-undef */
 
-export default () => {};
+import '@babel/polyfill';
+import isURL from 'validator/lib/isURL';
+
+export default () => {
+  const form = document.querySelector('.js-form');
+  const input = document.querySelector('.js-input');
+
+  const validate = (value, validator = (v) => v) => {
+    if (!validator(value)) {
+      input.classList.add('is-invalid');
+
+      return;
+    }
+
+    input.classList.remove('is-invalid');
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const url = formData.get('url');
+
+    validate(url, isURL);
+  };
+
+  const handleInput = (event) => validate(event.target.value);
+  const handleBlur = (event) => validate(event.target.value, isURL);
+
+  form.addEventListener('submit', handleSubmit);
+  input.addEventListener('input', handleInput);
+  input.addEventListener('blur', handleBlur);
+};
