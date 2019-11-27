5b35ebb8b5bbee78d4d18ac85710f6a706fb39cb
Rustam Yusupov <rustam.yusupov@gmail.com>
2019-11-27 22:37:57 +0200

    fix(app): change proxy protocol

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import parse from './parser';
 
 export default () => {
-  const corsProxy = 'http://cors-anywhere.herokuapp.com/';
+  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
   const form = document.querySelector('.js-form');
   const input = document.querySelector('.js-input');
   const button = document.querySelector('.js-button');
