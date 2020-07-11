94729b13eab39c5012f7ee4f07e7ebedf6c2006c
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-11 15:10:20 +0800

    feat(renders): get data from state

--- src/renders/validation.js
+++ src/renders/validation.js
@@ -1,12 +1,18 @@
-import { getOr } from 'lodash/fp';
+import {
+  flow,
+  get,
+  isEqual,
+  getOr,
+} from 'lodash/fp';
 
-const validation = (errors) => {
-  const error = getOr('', 0)(errors);
+const validation = (state) => {
+  const isInvalid = flow(get('form.validationState'), isEqual('invalid'))(state);
+  const error = getOr('', ['form', 'errors', 0])(state);
   const form = document.querySelector('.rss-form');
   const feedback = document.querySelector('.feedback');
 
-  form.elements.url.classList.toggle('is-invalid', error);
-  feedback.classList.toggle('text-danger', error);
+  form.elements.url.classList.toggle('is-invalid', isInvalid);
+  feedback.classList.toggle('text-danger', isInvalid);
   feedback.innerHTML = error;
 };
 
