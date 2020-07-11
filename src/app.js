8286bf7fed727cddd8a01da718b1c46bd29d1b4b
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-11 14:53:15 +0800

    feat(app): input and validate url

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
-const app = () => {};
+import { validation } from './watchers';
+import validate from './validator';
+
+const app = () => {
+  const state = {
+    form: {
+      errors: [],
+    },
+    feeds: [],
+    posts: [],
+  };
+
+  const form = document.querySelector('.rss-form');
+  const validationWatcher = validation(state);
+
+  const handleInput = (e) => {
+    const url = e.currentTarget.value;
+
+    validationWatcher.errors = validate(url, []);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  form.elements.url.addEventListener('input', handleInput);
+  form.addEventListener('submit', handleSubmit);
+};
 
 export default app;
