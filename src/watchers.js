9620af52a85f8e8736ca88af38a0aab41635b7bc
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-11 14:52:55 +0800

    feat(watchers): add validation

---
+++ src/watchers.js
@@ -0,0 +1,11 @@
+import onChange from 'on-change';
+
+import renderValidation from './renders/validation';
+
+export const form = (state) => onChange(state.form, () => {
+  // renderForm(state);
+});
+
+export const validation = (state) => onChange(state.form.errors, (_, value) => {
+  renderValidation(value);
+});
