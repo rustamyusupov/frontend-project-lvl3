1fd4a2c7e4721a937660e79b396ff6bffdca64cd
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-12 13:35:20 +0800

    feat(renders): clear url after request

--- src/renders/form.js
+++ src/renders/form.js
@@ -1,20 +1,25 @@
 const form = (value) => {
-  const { submit } = document.querySelector('.rss-form').elements;
+  const { url, submit } = document.querySelector('.rss-form').elements;
 
   switch (value) {
     case 'active':
-      submit.disabled = true;
+      url.disabled = false;
+      submit.disabled = false;
       break;
 
     case 'sending':
+      url.disabled = true;
       submit.disabled = true;
       break;
 
     case 'finished':
+      url.value = '';
+      url.disabled = false;
       submit.disabled = false;
       break;
 
     case 'failed':
+      url.disabled = false;
       submit.disabled = true;
       break;
 
