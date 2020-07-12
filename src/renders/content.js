5e47a9308118caa6cc4f2d59a7341b93178b421b
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-12 14:45:55 +0800

    feat(renders): init content

---
+++ src/renders/content.js
@@ -0,0 +1,5 @@
+const content = ({ feeds, posts }) => {
+  console.log({ feeds, posts });
+};
+
+export default content;
--- src/renders/index.js
+++ src/renders/index.js
@@ -1,2 +1,3 @@
+export { default as content } from './content';
 export { default as form } from './form';
 export { default as validation } from './validation';
