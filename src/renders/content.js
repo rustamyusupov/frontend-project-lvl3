6dc841b7140a01ad14e21d6abe769f95b3229613
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-19 13:30:34 +0800

    feat(renders): render content

--- src/renders/content.js
+++ src/renders/content.js
@@ -1,5 +1,18 @@
+import { map, flow, filter, join } from 'lodash/fp';
+
+const getItem = ({ link, title }) => `<div><a href=${link}>${title}</a></div>`;
+
 const content = ({ feeds, posts }) => {
-  console.log({ feeds, posts });
+  const feedsContainer = document.querySelector('.feeds');
+
+  const data = feeds.map(({ id, title }) => {
+    const header = `<h2>${title}</h2>`;
+    const items = flow(filter({ feedId: id }), map(getItem), join(''))(posts);
+
+    return `${header}${items}`;
+  });
+
+  feedsContainer.innerHTML = data;
 };
 
 export default content;
