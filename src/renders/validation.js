18476b1788efed0791ea25cf572be3e8857999b1
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-25 13:35:01 +0800

    feat(renders): render validation errors by i18next

--- src/renders/validation.js
+++ src/renders/validation.js
@@ -1,3 +1,5 @@
+import i18next from 'i18next';
+
 import {
   flow,
   get,
@@ -9,12 +11,12 @@ const validation = (value) => {
   const { url, submit } = document.querySelector('.rss-form').elements;
   const feedback = document.querySelector('.feedback');
   const isInvalid = flow(get('state'), isEqual('invalid'))(value);
-  const error = getOr('', ['errors', 0])(value);
+  const { type } = getOr({}, 'error')(value);
 
   url.classList.toggle('is-invalid', isInvalid);
   submit.disabled = isInvalid;
   feedback.classList.toggle('text-danger', isInvalid);
-  feedback.innerHTML = error;
+  feedback.innerHTML = i18next.t(type);
 };
 
 export default validation;
