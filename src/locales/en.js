ae1af4f35279ce899595c8c30534dc8fa4830cd1
Rustam Yusupov <rustam.yusupov@gmail.com>
2020-07-25 13:34:21 +0800

    feat(locales): add validation translations

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,5 +1,9 @@
 const en = {
-  translation: {},
+  translation: {
+    url: 'url must be a valid URL',
+    required: 'url is a required field',
+    notOneOf: 'url must be unnique',
+  },
 };
 
 export default en;
